d96cae69a7a7c6cdc315e725d1e9439aab2a7b15
Yash Dixit <yashdixit921@gmail.com>
2022-10-06 23:25:50 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// Constructor
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			items: [],
+			DataisLoaded: false
+		};
+  }
+  
+
+	// ComponentDidMount is used to
+	// execute the code
+	componentDidMount() {
+		fetch(
+"http://demo2211087.mockable.io/mock",{
+  method: "POST"
+  }
+)
+			.then((res) => res.json())
+			.then((json) => {
+				this.setState({
+					items: json.companies,
+					DataisLoaded: true
+				});
+			})
+	}
+	render() {
+    const { DataisLoaded, items } = this.state;
+    
+		if (!DataisLoaded) return <div>
+			<h1> Pleses wait some time.... </h1> </div> ;
+
+		return (
+		<div className = "App">
+			<h1> Fetched data from the api </h1> {
+				items.map((item, idx) => (
+				<ol key={idx}> 
+					Name: { item.name } &nbsp;
+          Email: { item.email } &nbsp;
+          Status: { item.status }
+				</ol>
+				))
+			}
+		</div>
+	);
+}
 }
 
 export default App;
